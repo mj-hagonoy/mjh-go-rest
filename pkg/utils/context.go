189a5a60a159573b6dfd5c8fb363972c202ab406
mjhagonoy <mjhagonoy.dev@gmail.com>
2022-02-16 15:14:00 +0800

    set test email

--- pkg/utils/context.go
+++ pkg/utils/context.go
@@ -1,7 +1,11 @@
 package utils
 
-import "context"
+import (
+	"context"
+
+	"github.com/mj-hagonoy/mjh-go-rest/pkg/config"
+)
 
 func GetUserEmail(c context.Context) string {
-	return "mhagonoy@gmail.com"
+	return config.GetConfig().Mail.EmaiFrom
 }
