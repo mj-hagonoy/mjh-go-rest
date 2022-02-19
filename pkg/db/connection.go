0e15260171b2197994e4525d68e9bb1fb1772e39
mjhagonoy <mjhagonoy.dev@gmail.com>
2022-02-19 13:21:39 +0800

    used sync.once for mongo client instance creation

--- pkg/db/connection.go
+++ pkg/db/connection.go
@@ -3,6 +3,7 @@ package db
 import (
 	"context"
 	"fmt"
+	"sync"
 	"time"
 
 	"github.com/mj-hagonoy/mjh-go-rest/pkg/config"
@@ -10,19 +11,26 @@ import (
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
-var mongoClient *mongo.Client
+type Database struct {
+	once   sync.Once
+	client *mongo.Client
+}
+
+var database Database
 
 func ConnectDB() (*mongo.Client, error) {
-	if mongoClient == nil {
-		var err error
-		ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-		defer cancel()
-		uri := fmt.Sprintf("mongodb://%s:%d", config.GetConfig().Database.Host, config.GetConfig().Database.Port)
-		mongoClient, err = mongo.Connect(ctx, options.Client().ApplyURI(uri))
-		if err != nil {
-			panic(err)
-		}
-	}
+	database.once.Do(createInstance)
 
-	return mongoClient, nil
+	return database.client, nil
+}
+
+func createInstance() {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	uri := fmt.Sprintf("mongodb://%s:%d", config.GetConfig().Database.Host, config.GetConfig().Database.Port)
+	mongoClient, err := mongo.Connect(ctx, options.Client().ApplyURI(uri))
+	if err != nil {
+		panic(err)
+	}
+	database.client = mongoClient
 }
