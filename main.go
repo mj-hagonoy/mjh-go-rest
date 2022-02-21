c45131494f5f1eefc22d724a84ac687535891f43
mjhagonoy <mjhagonoy.dev@gmail.com>
2022-02-21 21:43:56 +0800

    implemented webworker

--- main.go
+++ main.go
@@ -3,12 +3,8 @@ package main
 import (
 	"flag"
 	"fmt"
-	"log"
-	"net/http"
 	"os"
 
-	"github.com/gorilla/mux"
-	"github.com/mj-hagonoy/mjh-go-rest/handlers"
 	"github.com/mj-hagonoy/mjh-go-rest/pkg/config"
 	"github.com/mj-hagonoy/mjh-go-rest/pkg/logger"
 )
@@ -21,7 +17,8 @@ func main() {
 		worker := JobWorker{ProjectID: config.GetConfig().Messaging.GoogleCloud.ProjectID}
 		worker.Run()
 	case "web":
-		runRestService()
+		worker := WebWorker{}
+		worker.Run()
 	case WORKER_EMAIL:
 		worker := MailWorker{ProjectID: config.GetConfig().Messaging.GoogleCloud.ProjectID}
 		worker.Run()
@@ -40,27 +37,4 @@ func init() {
 	servType = *serviceType
 	os.Setenv("GOOGLE_APPLICATION_CREDENTIALS", config.GetConfig().Credentials.GoogleCloud)
 	logger.InitLoggers()
-}
-
-func runRestService() {
-	logger.InfoLogger.Println("HTTP service is adding handlers.")
-	router := mux.NewRouter()
-	router.HandleFunc("/", handlers.HomeHandler)
-
-	api1 := router.PathPrefix("/api/v1").Subrouter()
-
-	//Users
-	api1.HandleFunc("/users", handlers.GetAllUsersHandler).Methods(http.MethodGet)
-	api1.HandleFunc("/users/import", handlers.ImportUsersHandler).Methods(http.MethodPost)
-
-	//Jobs
-	api1.HandleFunc("/jobs/{id}", handlers.GetJob).Methods(http.MethodGet)
-
-	http.Handle("/", router)
-
-	logger.InfoLogger.Printf("HTTP service will listen to %s at port %d", config.GetConfig().Host, config.GetConfig().Port)
-	logger.InfoLogger.Println("HTTP service running...")
-	if err := http.ListenAndServe(fmt.Sprintf(":%d", config.GetConfig().Port), nil); err != nil {
-		log.Fatal(err)
-	}
 }
